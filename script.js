0a9cef7f99885afe2ab8c8f1fd1983becee96255
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-31 19:24:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
-    panel.classList.toggle('active');
+    removeActiveClasses();
+    panel.classList.add('active');
   });
 });
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+};
